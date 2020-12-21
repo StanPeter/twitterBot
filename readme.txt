fa7e1b2733c9642e804c871604c35ad92fe32065
StanPeter <stanislav.petr7@gmail.com>
2020-12-22 00:50:45 +0100

    improved readme + formatted coded

--- readme.txt
+++ readme.txt
@@ -1,16 +1,32 @@
-Twitter Bot project
+Information:
 
-I used this to learn more about twitter's API and this is the result. I eventually stopped as I'm not 
-as interested in Twitter(just curiosity)
 
-For now contains 3 main funcionalities ->
-Getting all tweets from your accounts, following people(who follow you) based on your criterias, liking tweets based on
-your criterias.
+    Twitter Bot project
 
-Manual:
+    I used this to learn more about twitter's API and this is the result. I eventually stopped as I'm not 
+    as interested in Twitter(just curiosity)
 
-0. create an environment inside cloned github folder
-1. activate env "source env/bin/activate" and install required modules "pip install -r requirements.txt"
-2. set up .env file and put there all required env variables(there are 4 :) )
-3. check out the functions, there are simple and you'll for sure understand, change arguments and launch the programme
+    For now contains 3 main funcionalities ->
+    Getting all tweets from your accounts, following people(who follow you) based on your criterias, liking tweets based on
+    your criterias.
 
+
+
+
+Instructions: (commands for Windows might differ)
+
+
+    1. Create a new environment                                 "  python -m venv venv  " 
+
+    2. Activate it                                              "  source venv/bin/activate  "
+
+    3. install required libraries by executing                  "  pip install -r requirements.txt  "
+    in terminal
+
+    4. create .env file and inside set up CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET variables which
+       you can find inside your twitter account in setting(exact setup might have changed so just search it up)
+
+    5. check out the functions, there are simple and you'll for sure understand, change arguments and execute the programme
+
+
+PS: Using Python3
--- twitter.py
+++ twitter.py
@@ -1,5 +1,7 @@
-#twitter Bot
-import tweepy, os, time
+# twitter Bot
+import tweepy
+import os
+import time
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,36 +12,40 @@ consumer_secret = os.getenv("CONSUMER_SECRET")
 access_token = os.getenv("ACCESS_TOKEN")
 access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
 
-#authorization for tweeter account, need to open a developer account there to get keys
+# authorization for tweeter account, need to open a developer account there to get keys
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-#if you have many tweets its better to put some restrictions
+
+# if you have many tweets its better to put some restrictions
 def get_published_tweets():
     public_tweets = api.home_timeline()
     for tweet in public_tweets:
         print(tweet.text)
 
-def limit_handler(cursor):  #if sent too many get requests sleep()
+
+def limit_handler(cursor):  # if sent too many get requests sleep()
     try:
         while True:
-            yield cursor.next() 
+            yield cursor.next()
     except tweepy.RateLimitError:
         time.sleep(1000)
 
-def follower_bot(name_to_follow): #Bot to follow people filtered by name
+
+def follower_bot(name_to_follow):  # Bot to follow people filtered by name
     for follower in limit_handler(tweepy.Cursor(api.followers).items()):
         if follower.name == name_to_follow:
             follower.follow()
             break
 
-#phrase/word you want to search for + number of tweets you want to like(set as favourite) containing the phrase/word
-def liking_bot(searched_phrase, num_of_tweets): 
+
+# phrase/word you want to search for + number of tweets you want to like(set as favourite) containing the phrase/word
+def liking_bot(searched_phrase, num_of_tweets):
     for tweet in limit_handler(tweepy.Cursor(api.search, searched_phrase).items(num_of_tweets)):
         try:
-            tweet.favorite()    #Tweet was liked
+            tweet.favorite()  # Tweet was liked
         except tweepy.TweepyError as e:
             print(e.reason)
         except StopIteration:
